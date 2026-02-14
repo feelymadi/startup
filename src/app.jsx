67d6936d5164c7c0a90c84a891e56e6eee8dfddd
Madilynn Feely <feelymadilynn@gmail.com>
2026-02-13 23:10:55 -0700

    try debug app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+
+
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { About } from './about/about';
 import { Charts } from './charts/charts';
@@ -12,65 +14,72 @@ import { Rank } from './rank/rank';
 
 
 
-
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="container-fluid bg-light text-dark pt-5 mt-4 min-vh-100">
-        <header className="container-fluid">
+      <div className="body">
+        <header className="container-fluid p-0">
           <nav className="navbar navbar-expand-md fixed-top navbar-light">
             <div className="container-fluid">
               <div className="navbar-brand">TuneChart<sup>&reg;</sup></div>
-
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mainNav"
-                aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#mainNav"
+                aria-controls="mainNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
                 <span className="navbar-toggler-icon"></span>
               </button>
-
               <div className="collapse navbar-collapse" id="mainNav">
                 <ul className="navbar-nav ms-auto">
                   <li className="nav-item">
-                    <NavLink className="nav-link active" to="/">Home</NavLink>
+                    <NavLink className="nav-link" to="/" end>Home</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="rank">Rank a Song</NavLink>
+                    <NavLink className="nav-link" to="/rank">Rank a Song</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="profile">Profile</NavLink>
+                    <NavLink className="nav-link" to="/profile">Profile</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="charts">Weekly Chart</NavLink>
+                    <NavLink className="nav-link" to="/charts">Weekly Chart</NavLink>
                   </li>
                   <li className="nav-item">
-                    <NavLink className="nav-link" to="about">About</NavLink>
+                    <NavLink className="nav-link" to="/about">About</NavLink>
                   </li>
                 </ul>
               </div>
             </div>
           </nav>
         </header>
-
+        <main>
           <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/charts' element={<Charts />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/rank' element={<Rank />} />
-            <Route path='*' element={<NotFound />} />
+            <Route path="/" element={<Login />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/charts" element={<Charts />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/rank" element={<Rank />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
- 
-
-
+        </main>
         <footer className="container-fluid">
-          <span className="text-reset">Author: Madilynn Feely </span><a href="https://github.com/feelymadi">GitHub</a>
+          <span className="text-reset">Author: Madilynn Feely </span>
+          <a href="https://github.com/feelymadi">GitHub</a>
         </footer>
-
       </div>
     </BrowserRouter>
   );
 }
 
 
+
 function NotFound() {
-  return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  return (
+    <div className="container-fluid text-center">
+      404: Return to sender. Address unknown.
+    </div>
+  );
 }
