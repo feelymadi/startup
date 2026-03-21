5067cfbabec3b5fb58347456e49ae8fb9d3d292f
Madilynn Feely <feelymadilynn@gmail.com>
2026-03-21 15:47:53 -0600

    fix vite configuration(took forever)

--- index.jsx
+++ index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './src/app';
+import App from './src/app.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
--- service/index.js
+++ service/index.js
@@ -9,8 +9,30 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello from TuneChart backend!' });
+app.get('/api/songs', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      title: 'Cherry Wine',
+      artist: 'Hozier',
+      album: 'Hozier',
+      ranking: 1,
+    },
+    {
+      id: 2,
+      title: 'The Night We Met',
+      artist: 'Lord Huron',
+      album: 'Strange Trails',
+      ranking: 2,
+    },
+    {
+      id: 3,
+      title: 'Work Song',
+      artist: 'Hozier',
+      album: 'Hozier',
+      ranking: 3,
+    },
+  ]);
 });
 
 app.listen(port, () => {
