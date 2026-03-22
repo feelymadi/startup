f19998cdcd6f67d1b732304e1a518632c6342a0a
Madilynn Feely <feelymadilynn@gmail.com>
2026-03-21 19:54:06 -0600

    update credentials

--- src/rank/rank.jsx
+++ src/rank/rank.jsx
@@ -56,6 +56,7 @@ export function Rank({ songs, setSongs, user }) {
         headers: {
           'Content-Type': 'application/json',
         },
+        credentials: 'include',
         body: JSON.stringify(ranking),
       });
 
