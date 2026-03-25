352cdf6a68dccd1fa23ac6aa168f95971eae2ba5
Madilynn Feely <feelymadilynn@gmail.com>
2026-03-24 20:59:51 -0600

    set up mongo

--- service/index.js
+++ service/index.js
@@ -1,3 +1,13 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('tuneChart');
+const userCollection = db.collection('user');
+const scoreCollection = db.collection('score');
+
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcryptjs');
